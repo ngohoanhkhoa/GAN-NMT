Fix: Calculate validation accuracy; logsoftmax -> softmax, binary_crossentropy -> categorial_crossentropy

nmtpy/mainloop_GAN.py
@@ -321,7 +321,7 @@ class MainLoop(object):
     
             probs = self.discriminator.get_probs_valid(*batch_discriminator)
             probs = np.array(probs)*np.array(batch_discriminator[2])
-            probs = probs.sum(0)
+            probs = probs.sum(1)
             true_num= sum(1 for prob in probs if prob > 0.5)
             prob_true.append(1 - (true_num/len(probs)))
             

nmtpy/mainloop_discriminator.py
@@ -166,7 +166,6 @@ class MainLoop(object):
             loss = self.model.train_batch(*batch_discriminator)
             batch_losses.append(loss)
             self.__send_stats(self.uctr, train_loss=loss)
-            
 
             # Verbose
             if self.uctr % self.f_verbose == 0:
@@ -219,15 +218,15 @@ class MainLoop(object):
             self.vctr += 1
 
             # Compute validation loss
-            self.model.set_dropout(False)
+#            self.model.set_dropout(False)
             cur_loss = self.model.val_loss()
-            self.model.set_dropout(True)
+#            self.model.set_dropout(True)
             
             # Add val_loss
             self.valid_metrics['loss'].append(cur_loss)
             
             # Print validation loss
-            self.__print("Validation %2d - LOSS = %.3f (PPL: %.3f)" % (self.vctr, cur_loss, np.exp(cur_loss)))
+            self.__print("Validation %2d - ACC = %.3f (LOSS: %.3f)" % (self.vctr, 1.0 - cur_loss, cur_loss) )
             
             f_valid_out = None
             if self.valid_save_hyp:

nmtpy/models/attention.py
@@ -491,7 +491,7 @@ class Model(BaseModel):
             logit = tensor.dot(logit, self.tparams['Wemb_dec'].T)
 
         # compute the logsoftmax
-        next_log_probs = tensor.nnet.logsoftmax(logit)
+        next_log_probs = tensor.nnet.softmax(logit)
 
         # Sample from the softmax distribution
         # NOTE: We never use sampling and it incurs performance penalty
@@ -544,7 +544,7 @@ class Model(BaseModel):
 
             else:
                 # Multinomial sampling        
-                nw = next_log_p[0].argmax()
+                nw = next_log_p[0][0]
                                      
             next_word = np.array([nw], dtype=INT)
 

nmtpy/models/cnn_discriminator.py
@@ -307,10 +307,12 @@ class Model(BaseModel):
         probs = tensor.clip(probs, 1e-7, 1.0 - 1e-7)
         
         # Khoa: Get binary cross entropy
-        cost = tensor.nnet.binary_crossentropy(probs, label)
-        
+#        cost = tensor.nnet.binary_crossentropy(probs, label)
         # Khoa: The same value in cols, so just take max
-        cost = cost.max(1)
+#        cost = cost.max(1)
+
+        # Khoa: Get categorical crossentropy
+        cost = tensor.nnet.categorical_crossentropy(probs, label)
         
         self.get_probs_valid = theano.function(list(self.inputs.values()), probs, on_unused_input='ignore')
         
@@ -634,7 +636,7 @@ class Model(BaseModel):
             probs = self.get_probs_valid(*batch_discriminator)
             probs_len = len(probs)
             probs = np.array(probs)*np.array(list(data.values())[4])
-            probs = probs.sum(0)
+            probs = probs.sum(1)
             true_num= sum(1 for prob in probs if prob > 0.5)
             prob_true.append(1 - (true_num/probs_len))
         if mean: